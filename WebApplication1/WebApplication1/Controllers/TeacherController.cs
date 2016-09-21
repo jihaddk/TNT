9ea2b8d2e8fa156dc6fc32b11804a0f48d0a6e65
Z6JHQ <Z6JHQ@H51307>
2016-09-21 10:59:37 +0200

    Test

---
+++ WebApplication1/WebApplication1/Controllers/TeacherController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TeacherController : ApiController
+    {
+        public Teacher Get(int id)
+        {
+            List<Student> students = new List<Student>();
+            for (int i = 0; i < 10; i++)
+            {
+                students.Add(new Student
+                {
+                    Id = i,
+                    Name = "Student" + i
+                });
+            }
+
+            Teacher teacher = new Teacher
+            {
+                Id = 1,
+                Name = "Teacher",
+                Students = students
+
+            };
+
+            return teacher;
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+
+            List<Student> students = new List<Student>();
+            for (int i = 0; i < 10; i++)
+            {
+                students.Add(new Student
+                {
+                    Id = i,
+                    Name = "Student" + i
+                });
+            }
+
+            Teacher teacher = new Teacher
+            {
+                Id = 1,
+                Name = "Teacher",
+                Students = students
+
+            };
+
+            return teacher.Students.Select(s => s.Name);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
